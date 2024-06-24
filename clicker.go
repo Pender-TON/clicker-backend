36c84b8609f2c032270f6337665f1d03872ca3a5
Danir <davaleev@edu.hse.ru>
2024-06-24 18:24:05 +0100

    tap multiplier for NFT-holders

--- clicker.go
+++ clicker.go
@@ -65,16 +65,16 @@ func init() {
 }
 
 type UserStats struct {
-    AuthDate      *int      `json:"auth_date" bson:"auth_date"`
-    UserID        *int     `json:"id" bson:"userId"`
-    IsPremium     *bool    `json:"is_premium" bson:"is_premium"`
-    LanguageCode  *string  `json:"language_code" bson:"language_code"`
-    UserName      *string  `json:"username" bson:"username"`
-    Count         *int     `json:"count" bson:"count"`
-    Address       *string  `json:"address" bson:"address"`
-    Gems          *int     `json:"gems" bson:"gems"`
-    Multiplier    *int     `json:"multiplier" bson:"multiplier"`
-    TonBalance    *float64 `json:"tonBalance" bson:"tonBalance"`
+    AuthDate      int     `json:"auth_date" bson:"auth_date"`
+    UserID        int     `json:"id" bson:"id"`
+    IsPremium     bool    `json:"is_premium" bson:"is_premium"`
+    LanguageCode  string  `json:"language_code" bson:"language_code"`
+    UserName      string  `json:"username" bson:"username"`
+    Count         int     `json:"count" bson:"count"`
+    Address       string  `json:"address" bson:"address"`
+    Gems          int     `json:"gems" bson:"gems"`
+    Multiplier    int     `json:"multiplier" bson:"multiplier"`
+    TonBalance    float64 `json:"tonBalance" bson:"tonBalance"`
 }
 
 type VerifyRequest struct {
@@ -83,25 +83,25 @@ type VerifyRequest struct {
 }
 
 type UpdateRequest struct {
-	UserID   int    `json:"userId"`
+	UserID   int    `json:"id"`
 	UserName string `json:"userName"`
 	Count    int    `json:"count"`
 }
 
-type InitRequest struct {
-	UserID   int    `json:"userId"`
-	UserName string `json:"userName"`
-	Count    int    `json:"count"`
-}
+//type InitRequest struct {
+//	UserID   int    `json:"userId"`
+//	UserName string `json:"userName"`
+//	Count    int    `json:"count"`
+//}
 
 func main() {
 	router := mux.NewRouter()
 	router.Use(enableCors)
 	router.HandleFunc("/updateField", updateFieldHandler).Methods("POST", "OPTIONS")
-	router.HandleFunc("/createDocument", createDocumentHandler).Methods("POST", "OPTIONS")
 	router.HandleFunc("/getDocument", getDocumentHandler).Methods("GET", "OPTIONS")
 	router.HandleFunc("/verifySignature", verifySignatureHandler).Methods("POST", "OPTIONS")
-	router.HandleFunc("/dbInit", dbInitHandler).Methods("POST", "OPTIONS")
+    router.HandleFunc("/updateAddress", updateAddressHandler).Methods("POST", "OPTIONS")
+	//router.HandleFunc("/dbInit", dbInitHandler).Methods("POST", "OPTIONS")
 	router.HandleFunc("/getPosition", getPositionHandler).Methods("POST", "OPTIONS")
 
 	err := http.ListenAndServeTLS(":443", "cert.pem", "key.pem", router)
@@ -129,7 +129,7 @@ func enableCors(next http.Handler) http.Handler {
 
 // Extracted logic from updateFieldHandler
 func updateCount(ctx context.Context, userId int, count int) (int, error) {
-    filter := bson.M{"userId": userId}
+    filter := bson.M{"id": userId}
     var currentDoc struct {
         Count int `bson:"count"`
     }
@@ -154,6 +154,128 @@ func updateCount(ctx context.Context, userId int, count int) (int, error) {
     return count, nil
 }
 
+// Function to update the address field in the database
+func updateAddress(ctx context.Context, userId int, address string) (int, error) {
+    
+    type ApiResponse struct {
+        NftItems []struct {
+            Collection struct {
+                Address string `json:"address"`
+            } `json:"collection"`
+        } `json:"nft_items"`
+    }
+    filter := bson.M{"id": userId}
+    var currentDoc struct {
+        Address string `bson:"address"`
+        Multiplier int `bson:"multiplier"`
+    }
+    if address == "" {
+        update := bson.M{"$set": bson.M{
+            "address": address,
+            "multiplier": 1,
+        }}
+        _, err := clicksColl.UpdateOne(ctx, filter, update)
+        if err != nil {
+            return 0, err
+        }
+        return 1, nil // Return the current multiplier value
+    }
+    
+    url := fmt.Sprintf("https://tonapi.io/v2/accounts/%s/nfts", address)
+    resp, err := http.Get(url)
+    if err != nil {
+        return 0, fmt.Errorf("error validating address: %w", err)
+    }
+    //defer resp.Body.Close()
+
+    // Check if the address is valid based on the response status code
+    if resp.StatusCode != http.StatusOK {
+        return 0, fmt.Errorf("invalid address, received status code: %d", resp.StatusCode)
+    }
+    
+    var apiResp ApiResponse
+    err = json.NewDecoder(resp.Body).Decode(&apiResp)
+    if err != nil {
+        return 0, fmt.Errorf("error decoding response: %w", err)
+    }
+    resp.Body.Close()
+    //fmt.Println(apiResp)
+
+    found := false
+    for _, item := range apiResp.NftItems {
+        if item.Collection.Address == "0:182fed439ab4db02a71ac0bf28cab653fde7ebd380946a351cdf4f613f1f5d45" {
+            found = true
+            break
+        }
+    }
+
+    err = clicksColl.FindOne(ctx, filter).Decode(&currentDoc)
+    if err != nil {
+        return 0, err
+    }
+    if found {
+        // If a matching NFT is found, set multiplier to 2 along with updating the address
+        update := bson.M{"$set": bson.M{
+            "address": address,
+            "multiplier": 2,
+        }}
+        _, err = clicksColl.UpdateOne(ctx, filter, update)
+        if err != nil {
+            return 0, err
+        }
+        return currentDoc.Multiplier, nil // Return 2 as the new multiplier value
+    } else {
+        // If no matching NFT is found, only update the address
+        update := bson.M{"$set": bson.M{
+            "address": address,
+            "multiplier": 1,
+        }}
+        _, err = clicksColl.UpdateOne(ctx, filter, update)
+        if err != nil {
+            return 0, err
+        }
+        return currentDoc.Multiplier, nil // Return the current multiplier value
+    }
+
+}
+
+// Handler for updating the address
+func updateAddressHandler(w http.ResponseWriter, r *http.Request) {
+    var req struct {
+        UserID  int    `json:"id"`
+        Address string `json:"address"`
+    }
+    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    // Context from the request, or background if not available
+    ctx := r.Context()
+
+    mult, err := updateAddress(ctx, req.UserID, req.Address)
+    if err != nil {
+        // Handle error, possibly with different messages based on the error type
+        http.Error(w, "Failed to update address", http.StatusInternalServerError)
+        return
+    }
+    response := struct {
+        Multiplier int `json:"multiplier"`
+    }{
+        Multiplier: mult,
+    }
+
+    jsonResponse, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonResponse)
+}
+
 func updateFieldHandler(w http.ResponseWriter, r *http.Request) {
     var req UpdateRequest
     if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -175,32 +297,27 @@ func updateFieldHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func createDocument(UserStats UserStats) error {
-	document := bson.M{
-		"userId":   UserStats.UserID,
-		"userName": UserStats.UserName,
-		"count":    UserStats.Count,
-        "is_premium": UserStats.IsPremium,
-        "language_code": UserStats.LanguageCode,
-        "address": UserStats.Address,
-        "gems": UserStats.Gems,
-        "multiplier": UserStats.Multiplier,
-        "tonBalance": UserStats.TonBalance,
-	}
+    // Initialize document with zero-values for all fields
+    document := bson.M{
+        "id":             UserStats.UserID,
+        "userName":       UserStats.UserName,
+        "count":          UserStats.Count,
+        "is_premium":     UserStats.IsPremium,
+        "language_code":  UserStats.LanguageCode,
+        "address":        UserStats.Address,
+        "gems":           UserStats.Gems,
+        "multiplier":     1,
+        "tonBalance":     UserStats.TonBalance,
+    }
 
-	_, err := clicksColl.InsertOne(ctx, document)
-	return err
-}
+    result, err := clicksColl.InsertOne(ctx, document)
+    if err != nil {
+        log.Printf("Failed to create new document: %v\n", err)
+        return err
+    }
 
-func createDocumentHandler(w http.ResponseWriter, r *http.Request) {
-	var req InitRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Create the document in the database
-
-	w.WriteHeader(http.StatusCreated)
+    log.Printf("New document created with ID: %+v\n", result)
+    return nil
 }
 
 func getDocumentHandler(w http.ResponseWriter, r *http.Request) {
@@ -229,7 +346,7 @@ func getDocumentHandler(w http.ResponseWriter, r *http.Request) {
 
 func getPositionHandler(w http.ResponseWriter, r *http.Request) {
 	var requestData struct {
-		UserId int `json:"userId"`
+		UserId int `json:"id"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
@@ -251,11 +368,11 @@ func getPositionHandler(w http.ResponseWriter, r *http.Request) {
 
 	position := -1
 	for i, result := range results {
-		dbUserId, ok := result["userId"].(int32)
+		dbUserId, ok := result["id"].(int32)
 		if !ok {
-			dbUserId64, ok := result["userId"].(int64)
+			dbUserId64, ok := result["id"].(int64)
 			if !ok {
-				fmt.Printf("Unexpected type for userId: %T\n", result["userId"])
+				fmt.Printf("Unexpected type for id: %T\n", result["id"])
 				continue
 			}
 			dbUserId = int32(dbUserId64)
@@ -270,45 +387,23 @@ func getPositionHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]int{"position": position})
 }
+// Assuming clicksColl is your MongoDB collection and ctx is your context
+func dbInit(ctx context.Context, userId int) (UserStats, error) {
+    filter := bson.M{"id": userId}
+    var result UserStats // Initialize an empty UserStats struct to hold the result
 
-func dbInitHandler(w http.ResponseWriter, r *http.Request) {
-	var req InitRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+    err := clicksColl.FindOne(ctx, filter).Decode(&result)
+    if err != nil {
+        if err == mongo.ErrNoDocuments {
+            // Handle the case where no document is found
+            return UserStats{}, mongo.ErrNoDocuments
+        }
+        // Handle other potential errors
+        return UserStats{}, err
+    }
 
-	filter := bson.M{
-		"userId":   req.UserID,
-		"userName": req.UserName,
-	}
-
-	var result struct {
-		Count int `bson:"count"`
-	}
-	err := clicksColl.FindOne(ctx, filter).Decode(&result)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			userStats := UserStats{
-                UserID:   &req.UserID,
-                UserName: &req.UserName,
-                Count:    &req.Count,
-            }
-            err = createDocument(userStats)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			w.WriteHeader(http.StatusCreated)
-			return
-		}
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, result.Count)
+    // If the document was found and successfully decoded, return the result and nil for the error
+    return result, nil
 }
 
 func verifySignatureHandler(w http.ResponseWriter, r *http.Request) {
@@ -319,7 +414,7 @@ func verifySignatureHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    //log.Printf("Request body: %s", body)
+    log.Printf("Request body: %s", body)
 
     // Parse the URL-encoded data
     values, err := url.ParseQuery(string(body))
@@ -370,6 +465,7 @@ func verifySignatureHandler(w http.ResponseWriter, r *http.Request) {
 
     // Prepare the response map
     var user UserStats
+    var tempUser UserStats
     responseMap := make(map[string]interface{})
     for key := range values {
         if key == "chat_instance" || key == "chat_type" {
@@ -383,7 +479,7 @@ func verifySignatureHandler(w http.ResponseWriter, r *http.Request) {
                 return
             }
             responseMap[key] = intValue
-            user.AuthDate = &intValue
+            user.AuthDate = intValue
         } else {
             responseMap[key] = value
         }
@@ -396,13 +492,26 @@ func verifySignatureHandler(w http.ResponseWriter, r *http.Request) {
             return
         }
     }
-    var count int
-    count, err = updateCount(ctx, *user.UserID, 1)
-    if err != nil {
-        http.Error(w, "Error updating count", http.StatusInternalServerError)
+
+    
+    
+    // Log the body content
+    //log.Printf("Request body before dbInit: %s\n", string(bodyBytes))
+    if user.UserID == 0 {
+        log.Printf("Error: UserID is nil\n")
+        http.Error(w, "UserID is required", http.StatusBadRequest)
         return
     }
-    user.Count = &count
+    tempUser, err = dbInit(r.Context(), user.UserID)
+    if err != nil {
+        log.Printf("Creating new doc: %v\n", err) // Log the actual error
+        createDocument(user)
+        //http.Error(w, "Error updating count", http.StatusInternalServerError)
+        //return
+    } else {
+        user = tempUser
+    }
+
 
     jsonUser, err := json.Marshal(user)
     if err != nil {
